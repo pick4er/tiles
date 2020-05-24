10543d2204c04df05a611ac44db641feda3d92c6
pick4er <pick4er@gmail.com>
2020-05-24 10:29:47 +0300

    Add helpers folder initially. Add compareArrays and isUndefined helpers. Change typeof operator on isUndefined in field flux module. Change areIdsMatch logic to compareArrays. Add idsToMatch prop state field initially

--- src/flux/modules/field/index.ts
+++ src/flux/modules/field/index.ts
@@ -8,6 +8,7 @@ import type {
 } from 'types';
 
 import { createSelector } from 'reselect';
+import { isUndefined, compareArrays } from 'helpers'
 
 interface State {
   width?: number;
@@ -15,6 +16,7 @@ interface State {
   tiles?: OpenableTile[];
   values?: string[];
   isMatch?: boolean;
+  idsToMatch?: TileId[]
 }
 
 interface PayloadAction extends Action<string> {
@@ -27,6 +29,7 @@ const SET_VALUES = 'FIELD/SET_VALUES'
 const SET_WIDTH = 'FIELD/SET_WIDTH'
 const SET_HEIGHT = 'FIELD/SET_HEIGHT'
 const SET_IS_MATCH = 'FIELD/SET_IS_MATCH'
+const SET_IDS_TO_MATCH = 'FIELD/SET_IDS_TO_MATCH'
 
 const initialState: State = {
   width: undefined,
@@ -34,6 +37,7 @@ const initialState: State = {
   tiles: undefined,
   values: undefined,
   isMatch: undefined,
+  idsToMatch: undefined,
 }
 
 // Reducer
@@ -46,6 +50,11 @@ export default function reducer(
       return {
         ...state,
         tiles: payload
+      }
+    case SET_IDS_TO_MATCH:
+      return {
+        ...state,
+        idsToMatch: payload
       }
     case SET_VALUES:
       return {
@@ -90,6 +99,11 @@ export const selectIsMatch = createSelector(
   ({ isMatch }): boolean | undefined => isMatch
 )
 
+export const selectIdsToMatch = createSelector(
+  selectFieldModule,
+  ({ idsToMatch }): TileId[] | undefined => idsToMatch
+)
+
 export const selectTiles = createSelector(
   selectFieldModule,
   ({ tiles }): OpenableTile[] | undefined => tiles
@@ -98,7 +112,7 @@ export const selectTiles = createSelector(
 export const selectTilesIds = createSelector(
   selectTiles,
   (tiles): TileId[] | undefined => 
-    typeof tiles === 'undefined'
+    isUndefined(tiles)
       ? []
       : tiles.map(({ id }: OpenableTile) => id)
 )
@@ -111,9 +125,9 @@ export const selectTwoDimensionalTiles = createSelector(
     const twoDimensionalTiles: (OpenableTile[])[] = [];
 
     if (
-      typeof width === 'undefined' ||
-      typeof height === 'undefined' ||
-      typeof tiles === 'undefined'
+      isUndefined(width) ||
+      isUndefined(height) ||
+      isUndefined(tiles)
     ) {
       return twoDimensionalTiles
     }
@@ -143,7 +157,7 @@ export const selectValuesToIds = createSelector(
   (tilesIds, values): Record<TileValue, TileId[]> => {
     const valuesToIds: Record<TileValue, TileId[]> = {}
 
-    if (typeof tilesIds === 'undefined') {
+    if (isUndefined(tilesIds)) {
       return valuesToIds
     }
 
@@ -159,7 +173,7 @@ export const selectValuesToIds = createSelector(
       }
 
       // out of ids range
-      if (typeof secondIdentifier === 'undefined') {
+      if (isUndefined(secondIdentifier)) {
         // identifier must have a value couple
         valuesToIds[value] = valuesToIds[value].concat(
           firstIdentifier
@@ -243,8 +257,8 @@ export const initTiles: ActionCreator<
   const height = selectHeight(state)
 
   if (
-    typeof width === 'undefined' ||
-    typeof height === 'undefined'
+    isUndefined(width) ||
+    isUndefined(height)
   ) {
     throw new TypeError('Specify field dimensions first')
   }
@@ -294,32 +308,20 @@ export const toggleTile: ActionCreator<
   dispatch(setTiles(tiles))
 }
 
-export const areValuesMatch: ActionCreator<
+export const areIdsMatch: ActionCreator<
   ThunkAction<void, RootState, void, PayloadAction>
 > = (ids?: TileId[]) => (dispatch, getState) => {
   const idsToValues = selectIdsToValues(getState())
+  const valuesToIds = selectValuesToIds(getState())
 
-  if (typeof ids === 'undefined') {
+  if (isUndefined(ids)) {
     dispatch(setIsMatch(undefined))
     return
   }
 
-  let currentValue: TileValue = ''
-  for (let identifier of ids) {
-    const value = idsToValues[identifier]
-
-    if (!currentValue) {
-      currentValue = value
-    } else if (value === currentValue) {
-      continue
-    } else {
-      dispatch(setIsMatch(false))
-      return
-    }
-  }
-
-  dispatch(setIsMatch(true))
-  return
+  const valueToGuess = idsToValues[ids[0]]
+  const idsOfValue = valuesToIds[valueToGuess]
+  dispatch(setIsMatch(compareArrays(ids, idsOfValue)))
 }
 
 export const initField: ActionCreator<
